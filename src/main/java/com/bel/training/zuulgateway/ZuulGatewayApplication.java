8f86682ae477499bacb06071fd4431ce2f07ff74
shrikant-tirki <shrikanttirki@gmail.com>
2022-03-05 14:03:32 +0530

    first commit

---
+++ src/main/java/com/bel/training/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.bel.training.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+
+}
